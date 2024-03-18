ca5154ba850268dcd516220abc6cd22f675cdd46
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-18 12:49:18 +0900

    init

---
+++ src/test/java/hello/springbootcore/SpringbootCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbootcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
