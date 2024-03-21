1e04322618ae1dabc8d466736fd6e54e691ba5dd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-21 14:35:16 +0900

    feat: 자동 구성 프로젝트 생성

---
+++ spring-autoconfiguration/src/main/java/hello/springautoconfiguration/SpringAutoconfigurationApplication.java
@@ -0,0 +1,13 @@
+package hello.springautoconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAutoconfigurationApplication.class, args);
+	}
+
+}
---
+++ spring-autoconfiguration/src/test/java/hello/springautoconfiguration/SpringAutoconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springautoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutoconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
