6ec625ee780565ea06c25bbfa45ee3976abeca96
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-21 15:46:13 +0900

    feat: MemoryController, MemoryFinder를 빈으로 등록

---
+++ spring-autoconfiguration/src/main/java/hello/springautoconfiguration/memory/MemoryConfig.java
@@ -0,0 +1,21 @@
+package hello.springautoconfiguration.memory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MemoryController, MemoryFinder를 빈으로 등록한다.
+ */
+@Configuration
+public class MemoryConfig {
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+}
