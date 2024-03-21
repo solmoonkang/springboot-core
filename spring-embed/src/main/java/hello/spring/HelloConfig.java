3d14d6bc87575fd0ccf0f1e83b7792668c063afe
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-21 12:08:46 +0900

    chore: 컴포넌트 스캔 사용을 위해 Configuration 어노테이션 제거

--- spring-embed/src/main/java/hello/spring/HelloConfig.java
+++ spring-embed/src/main/java/hello/spring/HelloConfig.java
@@ -1,9 +1,7 @@
 package hello.spring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class HelloConfig {
 
     @Bean
