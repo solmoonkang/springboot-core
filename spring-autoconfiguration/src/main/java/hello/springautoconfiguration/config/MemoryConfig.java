c67c539659efb5c3e4602158f29d82918e1208b6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-21 15:51:01 +0900

    chore: 메모리 설정 등록 클래스 위치 변경

--- spring-autoconfiguration/src/main/java/hello/springautoconfiguration/config/MemoryConfig.java
+++ spring-autoconfiguration/src/main/java/hello/springautoconfiguration/config/MemoryConfig.java
@@ -1,5 +1,7 @@
-package hello.memory;
+package hello.springautoconfiguration.config;
 
+import hello.memory.MemoryController;
+import hello.memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
