bac3f767f0c22fd6cb6e2c4a2938e88a734408ee
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-25 12:23:34 +0900

    feat: 외부설정 프로젝트 생성

---
+++ spring-external/src/main/java/hello/springexternal/SpringExternalApplication.java
@@ -0,0 +1,13 @@
+package hello.springexternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExternalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExternalApplication.class, args);
+    }
+
+}
---
+++ spring-external/src/test/java/hello/springexternal/SpringExternalApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springexternal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExternalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
