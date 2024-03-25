6561abb693b40226a46df543fb9260f05c7df85c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-25 14:24:40 +0900

    test: 자바 시스템 속성 확인하도록 구현

---
+++ spring-external/src/test/java/hello/springexternal/external/JavaSystemProperties.java
@@ -0,0 +1,17 @@
+package hello.springexternal.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+@Slf4j
+public class JavaSystemProperties {
+
+    public static void main(String[] args) {
+        Properties properties = System.getProperties();
+
+        for (Object key : properties.keySet()) {
+            log.info("prop {}={}", key, System.getProperty(String.valueOf(key)));
+        }
+    }
+}
