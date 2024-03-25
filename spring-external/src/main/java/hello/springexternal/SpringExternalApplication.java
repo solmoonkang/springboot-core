72e39da998d5c892eb3bc430fce4f66cb6803d67
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-25 17:25:01 +0900

    feat: 설정 정보를 빈으로 등록해서 사용하도록 추가

--- spring-external/src/main/java/hello/springexternal/SpringExternalApplication.java
+++ spring-external/src/main/java/hello/springexternal/SpringExternalApplication.java
@@ -1,9 +1,12 @@
 package hello.springexternal;
 
+import hello.springexternal.config.MyDataSourceEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(MyDataSourceEnvConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.springexternal.datasource")
 public class SpringExternalApplication {
 
     public static void main(String[] args) {
