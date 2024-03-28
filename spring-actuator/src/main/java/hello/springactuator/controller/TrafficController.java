37d670b823bff2f81d0842c014a5749cc363f786
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-28 15:19:44 +0900

    feat: JVM 메모리 사용을 누적하는 코드 구현

--- spring-actuator/src/main/java/hello/springactuator/controller/TrafficController.java
+++ spring-actuator/src/main/java/hello/springactuator/controller/TrafficController.java
@@ -4,13 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @RestController
 public class TrafficController {
 
     @GetMapping("/cpu")
     public String cpu() {
-        log.info("cpu");
+        log.info("CPU");
 
         long value =0;
 
@@ -18,6 +21,19 @@ public class TrafficController {
             value++;
         }
 
-        return "ok value=" + value;
+        return "OK VALUE: " + value;
+    }
+
+    private List<String> list = new ArrayList<>();
+
+    @GetMapping("/jvm")
+    public String jvm() {
+        log.info("JVM");
+
+        for (int i = 0; i < 1000000; i++) {
+            list.add("HELLO JVM!" + i);
+        }
+
+        return "OK";
     }
 }
