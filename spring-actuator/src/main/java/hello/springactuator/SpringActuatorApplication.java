acbee04f7d6154c288d972d21f6b131419e9da5c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-28 16:28:24 +0900

    feat: OrderConfigV4를 사용하도록 설정

--- spring-actuator/src/main/java/hello/springactuator/SpringActuatorApplication.java
+++ spring-actuator/src/main/java/hello/springactuator/SpringActuatorApplication.java
@@ -1,13 +1,13 @@
 package hello.springactuator;
 
-import hello.springactuator.order.v3.OrderConfigV3;
+import hello.springactuator.order.v4.OrderConfigV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV3.class)
+@Import(OrderConfigV4.class)
 @SpringBootApplication(scanBasePackages = "hello.springactuator.controller")
 public class SpringActuatorApplication {
 
