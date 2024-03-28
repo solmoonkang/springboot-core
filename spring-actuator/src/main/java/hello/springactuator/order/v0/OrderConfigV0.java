3355c06ddfdef18f199c957c7326fca916f9fac5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-28 15:34:56 +0900

    chore: v0 패키지로 이동

--- spring-actuator/src/main/java/hello/springactuator/order/v0/OrderConfigV0.java
+++ spring-actuator/src/main/java/hello/springactuator/order/v0/OrderConfigV0.java
@@ -1,5 +1,6 @@
-package hello.springactuator.order;
+package hello.springactuator.order.v0;
 
+import hello.springactuator.order.OrderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- spring-actuator/src/main/java/hello/springactuator/order/v0/OrderServiceV0.java
+++ spring-actuator/src/main/java/hello/springactuator/order/v0/OrderServiceV0.java
@@ -1,5 +1,6 @@
-package hello.springactuator.order;
+package hello.springactuator.order.v0;
 
+import hello.springactuator.order.OrderService;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.AtomicInteger;
