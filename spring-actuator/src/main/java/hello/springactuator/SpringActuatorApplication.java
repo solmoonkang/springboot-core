dcb0d80e2dec8cda03e4adabda93ed2371940d06
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-28 16:36:19 +0900

    feat: StockConfigV2를 사용하도록 설정

--- spring-actuator/src/main/java/hello/springactuator/SpringActuatorApplication.java
+++ spring-actuator/src/main/java/hello/springactuator/SpringActuatorApplication.java
@@ -1,6 +1,6 @@
 package hello.springactuator;
 
-import hello.springactuator.order.gauge.StockConfigV1;
+import hello.springactuator.order.gauge.StockConfigV2;
 import hello.springactuator.order.v4.OrderConfigV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({OrderConfigV4.class, StockConfigV1.class})
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.springactuator.controller")
 public class SpringActuatorApplication {
 
