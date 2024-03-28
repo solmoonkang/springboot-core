41c4d8c06403312861e6e50658f0a2ae21368e09
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-28 15:38:27 +0900

    feat: OrderServiceV0를 사용하도록 설정, 컴포넌트 스캔 대상을 컨트롤러로 지정하도록 설정

--- spring-actuator/src/main/java/hello/springactuator/SpringActuatorApplication.java
+++ spring-actuator/src/main/java/hello/springactuator/SpringActuatorApplication.java
@@ -1,11 +1,14 @@
 package hello.springactuator;
 
+import hello.springactuator.controller.OrderController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(OrderController.class)
+@SpringBootApplication(scanBasePackages = "hello.springactuator.controller")
 public class SpringActuatorApplication {
 
 	public static void main(String[] args) {
