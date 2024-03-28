408736f00e481c9e0b143d27468846d7bf43788c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-28 16:21:18 +0900

    feat: OrderConfigV3 사용하도록 설정

--- spring-actuator/src/main/java/hello/springactuator/SpringActuatorApplication.java
+++ spring-actuator/src/main/java/hello/springactuator/SpringActuatorApplication.java
@@ -1,13 +1,13 @@
 package hello.springactuator;
 
-import hello.springactuator.order.v2.OrderConfigV2;
+import hello.springactuator.order.v3.OrderConfigV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV2.class)
+@Import(OrderConfigV3.class)
 @SpringBootApplication(scanBasePackages = "hello.springactuator.controller")
 public class SpringActuatorApplication {
 
