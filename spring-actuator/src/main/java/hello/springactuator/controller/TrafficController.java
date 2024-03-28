30b4a6da83184a2405051840cfb229dc252f092c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-28 15:17:23 +0900

    feat: CPU에 부하를 줄 수 있는 코드를 구현

---
+++ spring-actuator/src/main/java/hello/springactuator/controller/TrafficController.java
@@ -0,0 +1,23 @@
+package hello.springactuator.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TrafficController {
+
+    @GetMapping("/cpu")
+    public String cpu() {
+        log.info("cpu");
+
+        long value =0;
+
+        for (long i = 0; i < 100000000L; i++) {
+            value++;
+        }
+
+        return "ok value=" + value;
+    }
+}
