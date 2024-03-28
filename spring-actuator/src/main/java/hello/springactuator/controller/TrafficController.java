025aa18ce575a095b6b75f81e812f9d9d5ad3b61
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-28 15:22:05 +0900

    feat: 커넥션 풀을 고갈시키는 코드 구현

--- spring-actuator/src/main/java/hello/springactuator/controller/TrafficController.java
+++ spring-actuator/src/main/java/hello/springactuator/controller/TrafficController.java
@@ -1,15 +1,23 @@
 package hello.springactuator.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @RestController
 public class TrafficController {
+
+    @Autowired DataSource dataSource;
+
+    private List<String> list = new ArrayList<>();
 
     @GetMapping("/cpu")
     public String cpu() {
@@ -24,8 +32,6 @@ public class TrafficController {
         return "OK VALUE: " + value;
     }
 
-    private List<String> list = new ArrayList<>();
-
     @GetMapping("/jvm")
     public String jvm() {
         log.info("JVM");
@@ -33,6 +39,17 @@ public class TrafficController {
         for (int i = 0; i < 1000000; i++) {
             list.add("HELLO JVM!" + i);
         }
+
+        return "OK";
+    }
+
+    @GetMapping("/jdbc")
+    public String jdbc() throws SQLException {
+        log.info("JDBC");
+
+        Connection connection = dataSource.getConnection();
+        log.info("connection info={}", connection);
+        // conn.close();        // 커넥션을 닫지 않는다.
 
         return "OK";
     }
