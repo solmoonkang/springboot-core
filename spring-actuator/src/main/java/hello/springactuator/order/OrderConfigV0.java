d66b9421561103330a7678b52b3a0daf57085039
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-28 15:34:01 +0900

    feat: OrderService 빈을 직접 등록하도록 설정

---
+++ spring-actuator/src/main/java/hello/springactuator/order/OrderConfigV0.java
@@ -0,0 +1,13 @@
+package hello.springactuator.order;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV0 {
+
+    @Bean
+    OrderService orderService() {
+        return new OrderServiceV0();
+    }
+}
